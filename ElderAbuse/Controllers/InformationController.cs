ebe16936646c5ce6cf7b07d7a284068ca9df9b03
afzal moideen <lmc.thenextapps@gmail.com>
2018-09-22 15:06:23 +1000

    added solutions controller and removed unwanted code

--- ElderAbuse/Controllers/InformationController.cs
+++ ElderAbuse/Controllers/InformationController.cs
@@ -6,113 +6,16 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using ElderAbuse.Models;
 
 namespace ElderAbuse.Controllers
 {
     public class InformationController : Controller
     {
-        private HelpEldersDBEntities db = new HelpEldersDBEntities();
-
+       
         // GET: Information
         public ActionResult Index()
         {
             return View();
-        }
-
-        // GET: Information/Details/5
-        public ActionResult Details(int? id)
-        {
-            return View();
-        }
-
-        // GET: Information/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Information/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ResponseNumber,AbuseType")] ResponseType responseType)
-        {
-            if (ModelState.IsValid)
-            {
-                db.ResponseTypes.Add(responseType);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(responseType);
-        }
-
-        // GET: Information/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ResponseType responseType = db.ResponseTypes.Find(id);
-            if (responseType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(responseType);
-        }
-
-        // POST: Information/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ResponseNumber,AbuseType")] ResponseType responseType)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(responseType).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(responseType);
-        }
-
-        // GET: Information/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ResponseType responseType = db.ResponseTypes.Find(id);
-            if (responseType == null)
-            {
-                return HttpNotFound();
-            }
-            return View(responseType);
-        }
-
-        // POST: Information/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            ResponseType responseType = db.ResponseTypes.Find(id);
-            db.ResponseTypes.Remove(responseType);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
--- ElderAbuse/Controllers/SolutionsController.cs
+++ ElderAbuse/Controllers/SolutionsController.cs
@@ -6,127 +6,52 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using ElderAbuse.Models;
 
 namespace ElderAbuse.Controllers
 {
     public class SolutionsController : Controller
     {
-        private HelpEldersDBEntities db = new HelpEldersDBEntities();
+        
 
         // GET: Solutions
         public ActionResult Index()
         {
-            var responses = db.Responses.Include(r => r.Question);
-            return View(responses.ToList());
+            
+            return View();
         }
 
         // GET: Solutions/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            return View(response);
+           
+            return View();
         }
 
         // GET: Solutions/Create
         public ActionResult Create()
         {
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1");
+           
             return View();
         }
 
-        // POST: Solutions/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Responses.Add(response);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
-        }
+       
 
         // GET: Solutions/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
+           
+            return View();
         }
 
-        // POST: Solutions/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(response).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
-        }
+        
 
         // GET: Solutions/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            return View(response);
+          
+            return View();
         }
 
-        // POST: Solutions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Response response = db.Responses.Find(id);
-            db.Responses.Remove(response);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+       
     }
 }
