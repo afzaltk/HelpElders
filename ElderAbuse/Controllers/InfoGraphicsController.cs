8ddce9533bf8fd7cc0390ee7d7b6b930e50ec9e2
afzal moideen <lmc.thenextapps@gmail.com>
2018-09-22 15:01:50 +1000

    added solutions controller

--- ElderAbuse/Controllers/InfoGraphicsController.cs
+++ ElderAbuse/Controllers/InfoGraphicsController.cs
@@ -6,13 +6,12 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using ElderAbuse.Models;
 
 namespace ElderAbuse.Controllers
 {
     public class InfoGraphicsController : Controller
     {
-        private HelpEldersDBEntities db = new HelpEldersDBEntities();
+     
 
         // GET: InfoGraphics
         public ActionResult Index()
@@ -21,112 +20,7 @@ namespace ElderAbuse.Controllers
             return View();
         }
 
-        // GET: InfoGraphics/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            return View(response);
-        }
+       
 
-        // GET: InfoGraphics/Create
-        public ActionResult Create()
-        {
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1");
-            return View();
-        }
-
-        // POST: InfoGraphics/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Responses.Add(response);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
-        }
-
-        // GET: InfoGraphics/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
-        }
-
-        // POST: InfoGraphics/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(response).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
-            return View(response);
-        }
-
-        // GET: InfoGraphics/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Response response = db.Responses.Find(id);
-            if (response == null)
-            {
-                return HttpNotFound();
-            }
-            return View(response);
-        }
-
-        // POST: InfoGraphics/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Response response = db.Responses.Find(id);
-            db.Responses.Remove(response);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
---
+++ ElderAbuse/Controllers/SolutionsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ElderAbuse.Models;
+
+namespace ElderAbuse.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        private HelpEldersDBEntities db = new HelpEldersDBEntities();
+
+        // GET: Solutions
+        public ActionResult Index()
+        {
+            var responses = db.Responses.Include(r => r.Question);
+            return View(responses.ToList());
+        }
+
+        // GET: Solutions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // GET: Solutions/Create
+        public ActionResult Create()
+        {
+            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1");
+            return View();
+        }
+
+        // POST: Solutions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Responses.Add(response);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
+            return View(response);
+        }
+
+        // GET: Solutions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
+            return View(response);
+        }
+
+        // POST: Solutions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ResponseId,ResponseNumber,QuestionId,Answer")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(response).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.QuestionId = new SelectList(db.Questions, "QuestionId", "Question1", response.QuestionId);
+            return View(response);
+        }
+
+        // GET: Solutions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // POST: Solutions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Response response = db.Responses.Find(id);
+            db.Responses.Remove(response);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
